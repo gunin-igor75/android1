de671e8cc03b7b3c466f926cbc64b467f4d79d60
Igor Gunin <gunin-igor75@yandex.ru>
2024-04-26 11:23:04 +0600

    #403 - Реализация разметки home_fragment

--- app/src/main/java/ru/it_cron/android1/presentation/home/HomeFragment.kt
+++ app/src/main/java/ru/it_cron/android1/presentation/home/HomeFragment.kt
@@ -1,23 +1,23 @@
 package ru.it_cron.android1.presentation.home
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.it_cron.android1.R
-
-
-
+import androidx.fragment.app.Fragment
+import ru.it_cron.android1.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding
+        get() = _binding?: throw IllegalStateException("FragmentHomeBinding is null")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
