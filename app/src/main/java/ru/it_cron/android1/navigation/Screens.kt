e4fc219376dc51c926f10e6170129dffad54429f
Igor Gunin <gunin-igor75@yandex.ru>
2024-05-06 08:39:47 +0600

    #415 -  Фикс case_screen

--- app/src/main/java/ru/it_cron/android1/navigation/Screens.kt
+++ app/src/main/java/ru/it_cron/android1/navigation/Screens.kt
@@ -1,7 +1,7 @@
 package ru.it_cron.android1.navigation
 
 import com.github.terrakok.cicerone.androidx.FragmentScreen
-import ru.it_cron.android1.CaseFragment
+import ru.it_cron.android1.presentation.case_screen.CaseFragment
 import ru.it_cron.android1.presentation.cases.CasesFragment
 import ru.it_cron.android1.presentation.company.CompanyFragment
 import ru.it_cron.android1.presentation.contacts.ContactsFragment
--- app/src/main/java/ru/it_cron/android1/presentation/case_screen/CaseFragment.kt
+++ app/src/main/java/ru/it_cron/android1/presentation/case_screen/CaseFragment.kt
@@ -1,10 +1,11 @@
-package ru.it_cron.android1
+package ru.it_cron.android1.presentation.case_screen
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import ru.it_cron.android1.R
 import ru.it_cron.android1.databinding.FragmentCaseBinding
 import ru.it_cron.android1.presentation.extension.arguments
 
